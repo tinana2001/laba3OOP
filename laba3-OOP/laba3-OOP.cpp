9c8abe5e5592282f3f5a09be7d7579b2c8b8ba58
nastya <tinana2013@gmail.com>
2021-02-01 18:23:19 +0500

    Добавила классы Cat,Dog,Object

--- laba3-OOP/laba3-OOP.cpp
+++ laba3-OOP/laba3-OOP.cpp
@@ -2,9 +2,38 @@
 //
 
 #include <iostream>
+using namespace std;
+class Object {
+public:
+	Object() {
+		cout << "Object()\n";
+	}
+	virtual ~Object() {
+		cout << "~Object()\n";
+	}
+};
+class Cat :public Object {
+public:
+	Cat() {
+		cout << "Cat()\n";
+	}
+	~Cat() {
+		cout << "~Dog()\n";
+	}
 
+};
+class Dog :public Object {
+public:
+	Dog() {
+		cout << "Dog()\n";
+	}
+	~Dog() {
+		cout << "~Dog()\n";
+	}
+
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
 
