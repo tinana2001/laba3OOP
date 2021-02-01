0320e8f9a842c1f1e1f59eb3f7b6f15954ec493a
nastya <tinana2013@gmail.com>
2021-02-01 18:17:52 +0500

    Добавьте файлы проекта.

---
+++ laba3-OOP/laba3-OOP.cpp
@@ -0,0 +1,10 @@
+﻿// laba3-OOP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
